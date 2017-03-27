21b52fa56e1ad1c2a43d305dc7d736a818b9e658
Aljosa Musikic <mr.aljosa@sams-iphone.ait-sydney.net.au>
2017-03-27 15:20:31 +1100

    created comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,10 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    # @post = Post.find(params[:id])
+    @comment = Comment.new
+    #return an array of all comments
+    @comments = @post.comments
   end
 
   # GET /posts/new
